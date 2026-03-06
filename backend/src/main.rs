154dbd602cdf1d21d1bda655dece1e2114a2d7df
drag <drag0n1zed@outlook.com>
2026-03-06 19:01:07 +0800

    step 2: set up endpoints

--- backend/src/main.rs
+++ backend/src/main.rs
@@ -1,27 +1,50 @@
-use std::error::Error;
+use std::sync::Arc;
 
-use sqlx::postgres::PgPoolOptions;
+use axum::{Router, extract::State, http::StatusCode, routing::get};
+use color_eyre::eyre::Result;
+use sqlx::{Pool, Postgres, postgres::PgPoolOptions};
+
+struct AppState {
+    pool: Pool<Postgres>,
+}
 
 #[tokio::main]
-async fn main() -> Result<(), Box<dyn Error>> {
+async fn main() -> Result<()> {
+    color_eyre::install()?;
     dotenvy::dotenv()?;
 
+    // Set up state
+    let shared_state = Arc::new(AppState {
+        pool: init_db().await?,
+    });
+
+    let app = Router::new()
+        .route("/user-count", get(get_user_count))
+        .route("/health", get(|| async { StatusCode::OK }))
+        .with_state(shared_state);
+
+    let listener = tokio::net::TcpListener::bind("0.0.0.0:8080").await?;
+    axum::serve(listener, app).await?;
+
+    Ok(())
+}
+
+async fn init_db() -> Result<Pool<Postgres>> {
     let pool = PgPoolOptions::new()
         .max_connections(5)
-        .connect(
-            std::env::var("DATABASE_URL")
-                .expect("DATABASE_URL must be set in backend/.env")
-                .as_str(),
-        )
+        .connect(std::env::var("DATABASE_URL")?.as_str())
         .await?;
 
     sqlx::migrate!().run(&pool).await?;
 
-    let result = sqlx::query_scalar!("SELECT COUNT(*) FROM users")
-        .fetch_one(&pool)
-        .await?
-        .ok_or("no result. something is set up wrong")?;
+    Ok(pool)
+}
 
-    println!("{result}");
-    Ok(())
+async fn get_user_count(State(state): State<Arc<AppState>>) -> Result<String, StatusCode> {
+    let result = sqlx::query_scalar!("SELECT COUNT(*) FROM users")
+        .fetch_one(&state.pool)
+        .await
+        .map_err(|_| StatusCode::INTERNAL_SERVER_ERROR)?
+        .ok_or(StatusCode::INTERNAL_SERVER_ERROR)?;
+    Ok(result.to_string())
 }
