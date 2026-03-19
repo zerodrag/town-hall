c7320aa12880a3403451ddb9746d62304c2b7a5a
drag <drag0n1zed@outlook.com>
2026-03-19 20:03:38 +0800

    backend: add cors layer

--- backend/src/main.rs
+++ backend/src/main.rs
@@ -4,8 +4,10 @@ mod state;
 
 use anyhow::Result;
 
+use http::{HeaderValue, Method, header};
 use sqlx::PgPool;
 pub use state::AppState;
+use tower_http::cors::CorsLayer;
 use tower_sessions::{SessionManagerLayer, cookie::SameSite};
 use tower_sessions_sqlx_store::PostgresStore;
 
@@ -18,9 +20,14 @@ async fn main() -> Result<()> {
 
     let state = AppState::new().await?;
     let session_layer = session_layer(state.db_pool.clone()).await?;
+    let cors_layer = cors_layer().await?;
 
     let listener = tokio::net::TcpListener::bind("0.0.0.0:3000").await?;
-    let app = router::root().await?.with_state(state).layer(session_layer);
+    let app = router::root()
+        .await?
+        .with_state(state)
+        .layer(session_layer)
+        .layer(cors_layer);
     axum::serve(listener, app)
         .with_graceful_shutdown(shutdown_signal())
         .await?;
@@ -31,5 +38,14 @@ async fn session_layer(db_pool: PgPool) -> Result<SessionManagerLayer<PostgresSt
     let session_store = PostgresStore::new(db_pool);
     session_store.migrate().await?;
     let layer = SessionManagerLayer::new(session_store).with_same_site(SameSite::Lax);
+    Ok(layer)
+}
+
+async fn cors_layer() -> Result<CorsLayer> {
+    let layer = CorsLayer::new()
+        .allow_origin("http://localhost:5173".parse::<HeaderValue>().unwrap())
+        .allow_methods([Method::GET, Method::POST])
+        .allow_headers([header::CONTENT_TYPE])
+        .allow_credentials(true);
     Ok(layer)
 }
