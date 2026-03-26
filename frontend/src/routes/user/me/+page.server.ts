ef3f3954ab29518e4ca5fe40588c8f2de231c9eb
drag <drag0n1zed@outlook.com>
2026-03-26 21:51:37 +0800

    frontend: add `user/me`

---
+++ frontend/src/routes/user/me/+page.server.ts
@@ -0,0 +1,9 @@
+import { error, redirect } from '@sveltejs/kit';
+
+export const load = async ({ parent }) => {
+	const { user } = await parent();
+	if (!user) {
+		error(401, 'Not logged in');
+	}
+	redirect(302, `/user/${user.handle}`);
+};
